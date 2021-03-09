c20a307dbf2678d068553ec122e847fcd89ec73d
viky1912 <80360449+viky1912@users.noreply.github.com>
2021-03-09 17:49:51 -0500

    se creo la clase Usuarios

---
+++ ProyectoDesarrolo/src/vista/Usuario.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vista;
+
+/**
+ *
+ * @author Vicky
+ */
+public class Usuario {
+    private String ID;
+    private String Nombre;
+    private String Apellido;
+    private String Telefono;
+    private String Direccion;
+    private String Contraseña;
+    
+    public Usuario(){
+        
+    }
+    public 
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public String getApellido() {
+        return Apellido;
+    }
+
+    public void setApellido(String Apellido) {
+        this.Apellido = Apellido;
+    }
+
+    public String getTelefono() {
+        return Telefono;
+    }
+
+    public void setTelefono(String Telefono) {
+        this.Telefono = Telefono;
+    }
+
+    public String getDireccion() {
+        return Direccion;
+    }
+
+    public void setDireccion(String Direccion) {
+        this.Direccion = Direccion;
+    }
+
+    public String getContraseña() {
+        return Contraseña;
+    }
+
+    public void setContraseña(String Contraseña) {
+        this.Contraseña = Contraseña;
+    }
+    
+    
+    
+    
+}
