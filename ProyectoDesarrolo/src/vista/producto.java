9382f8f89b64dd1e5de7b86ed3eeec09a080283e
Danidiaz27 <79545729+Danidiaz27@users.noreply.github.com>
2021-03-16 19:31:46 -0500

    16/03/2020
    se creo la clase productos

--- ProyectoDesarrolo/src/vista/producto.java
+++ ProyectoDesarrolo/src/vista/producto.java
@@ -10,5 +10,58 @@ package vista;
  * @author Daniela
  */
 public class producto {
-    
+    private String Nombre;
+    private String  codigo;
+    private int valor;
+    private int ValorDescuento;
+    private String FechaVencimiento;
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+
+    public int getValorDescuento() {
+        return ValorDescuento;
+    }
+
+    public void setValorDescuento(int ValorDescuento) {
+        this.ValorDescuento = ValorDescuento;
+    }
+
+    public String getFechaVencimiento() {
+        return FechaVencimiento;
+    }
+
+    public void setFechaVencimiento(String FechaVencimiento) {
+        this.FechaVencimiento = FechaVencimiento;
+    }
+
+    public producto(String Nombre, String codigo, int valor, int ValorDescuento, String FechaVencimiento) {
+        this.Nombre = Nombre;
+        this.codigo = codigo;
+        this.valor = valor;
+        this.ValorDescuento = ValorDescuento;
+        this.FechaVencimiento = FechaVencimiento;
+    }
+            
 }
